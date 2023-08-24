2e18ec2604491d7e0f1f74f4d1c8b0dd3bd53d6d
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-08-24 17:23:11 +0200

    Added code to cpu monitoring

--- monitoring/gpu_monitoring.py
+++ monitoring/gpu_monitoring.py
@@ -23,6 +23,7 @@ class GpuMonitoring:
             )
         
         self.file_stream = open(f'{monitoring_file_path}/monitoring_stream.csv', 'a')
+        self.current_video: str = ''
         
     @suppress(Exception)
     def start(self) -> None:
@@ -30,6 +31,7 @@ class GpuMonitoring:
         # TODO make sure initial energy is read for gpu
         self.start_time = pd.Timestamp('now')
         self.last_measured_time = pd.Timestamp('now')
+        self.__write_to_file('start', use_header=True)
         self.scheduler.start()
         
 
@@ -39,20 +41,8 @@ class GpuMonitoring:
         
 
     def monitor_gpu(self):
-        # taken from codecarbon emission tracker
         last_measurement_time_delta = pd.Timestamp('now') - self.last_measured_time
-        warning_duration = self.monitoring_interval_in_secs * 3
-        # if last_measurement_time_delta > warning_duration:
-        #     warn_msg = (
-        #         "Background scheduler didn't run for a long period"
-        #         + " (%ds), results might be inaccurate"
-        #     )
-        #     print(warn_msg)
-
-        df: pd.DataFrame = self.gpu_metadata_handler.get_update_as_pandas_df()
-        print(f'print df {df}')
-        df.to_csv(self.file_stream)
-        self.file_stream.flush()
+        self.__write_to_file(self.current_video)
         self.last_measured_time = pd.Timestamp('now')
 
     def __enter__(self):
@@ -62,9 +52,14 @@ class GpuMonitoring:
     def __exit__(self, exc_type, exc_value, tb) -> None:
         self.stop()
 
+    def __write_to_file(self, video_name: str, use_header: bool = False) -> None:
+        df: pd.DataFrame = self.gpu_metadata_handler.get_update_as_pandas_df()
+        df['current_video'] = video_name
+        df.to_csv(self.file_stream, header=use_header)
+        self.file_stream.flush()
+
 
 if __name__ == '__main__':
-    print('hehehe')
     monitoring = GpuMonitoring('.', 1)
     monitoring.start()
     sleep(5)
