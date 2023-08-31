f31d31c5b4d47d48f45429b8eebffedd4b422abc
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-08-31 14:21:30 +0200

    Added video source to download script

--- download_full_input_files.py
+++ download_full_input_files.py
@@ -9,13 +9,14 @@ from multiprocessing.pool import ThreadPool
 all_video_urls: dict[str, str] = {
     'AncientThought': 'https://www.youtube.com/watch?v=60Nkt4JvteE',
     'Eldorado': 'https://www.youtube.com/watch?v=qp54o2_HQvM',
-    # 'Indoor Soccer': 'https://www.youtube.com/watch?v=H_TzQ05qM_Y',
-    # 'Lifting Off': 'https://www.youtube.com/watch?v=32u4MhmHs9s',
-    # 'LiveUntouched': 'https://www.youtube.com/watch?v=J9sshEhi0Pc',
-    # 'Moment of Intensity': 'https://www.youtube.com/watch?v=JLTmjZmqiNc',
-    # 'Seconds That Count': 'https://www.youtube.com/watch?v=xMKmFU50deA',
-    # 'Skateboading': 'https://www.youtube.com/watch?v=xp0-mVmnQJk',
-    # 'Unspoken Friend': 'https://www.youtube.com/watch?v=K-MGvhjD4tg',
+    'Indoor Soccer': 'https://www.youtube.com/watch?v=H_TzQ05qM_Y',
+    'Lifting Off': 'https://www.youtube.com/watch?v=32u4MhmHs9s',
+    'LiveUntouched': 'https://www.youtube.com/watch?v=J9sshEhi0Pc',
+    'Moment of Intensity': 'https://www.youtube.com/watch?v=JLTmjZmqiNc',
+    'Seconds That Count': 'https://www.youtube.com/watch?v=xMKmFU50deA',
+    'Skateboading': 'https://www.youtube.com/watch?v=xp0-mVmnQJk',
+    'Unspoken Friend': 'https://www.youtube.com/watch?v=K-MGvhjD4tg',
+    'Portugal': 'https://www.youtube.com/watch?v=pIWVqgtg-Js',
 }
 
 def download_video(url):
@@ -41,7 +42,7 @@ def convert_webm_to_mp4():
 
     for video in video_file_paths:
         output_name: str = video.split(' HEVC')[0].replace(' ', '_')
-        ffmpeg_call = f'ffmpeg -y -i "{video}" {output_name}.mp4'
+        ffmpeg_call = f'ffmpeg -hwaccel cuda -y -i "{video}" {output_name}.mp4'
         
         os.system(ffmpeg_call)
         
@@ -57,7 +58,7 @@ def rename_videos(dir_path: str = '.'):
         
         
 if __name__ == '__main__':
-    download_parallel(list(all_video_urls.values()))
+    # download_parallel(list(all_video_urls.values()))
     convert_webm_to_mp4()
     rename_videos()
     
