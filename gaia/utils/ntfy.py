4e755195b787d5ed6558758b37227a8351c5b183
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-09-08 09:00:41 +0000

    Removed guard from ntfy class

--- gaia/utils/ntfy.py
+++ gaia/utils/ntfy.py
@@ -3,14 +3,8 @@ import os
 
 NTFY_BASE_URL: str = '3.76.6.7'
 
-TOPICS: list[str] = [
-    'encoding'
-]
-
-
 def send_ntfy(topic: str, message: str) -> str:
-    if topic in TOPICS:
-        os.system(f'curl -d "{message}" {NTFY_BASE_URL}/{topic}')
+    os.system(f'curl -d "{message}" {NTFY_BASE_URL}/{topic}')
 
 
 if __name__ == '__main__':
