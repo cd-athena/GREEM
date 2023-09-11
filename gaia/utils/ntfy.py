d88d050626f9657d985538bb15c0fee7e8be9f61
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-09-11 12:55:46 +0200

    added cli parser field to ntfy

--- gaia/utils/ntfy.py
+++ gaia/utils/ntfy.py
@@ -1,10 +1,14 @@
 
 import os
+from gaia.utils.benchmark import CLI_PARSER
+
+USE_NTFY: bool = CLI_PARSER.is_ntfy_enabled()
 
 NTFY_BASE_URL: str = '3.76.6.7'
 
 def send_ntfy(topic: str, message: str) -> str:
-    os.system(f'curl -d "{message}" {NTFY_BASE_URL}/{topic}')
+    if USE_NTFY:
+        os.system(f'curl -d "{message}" {NTFY_BASE_URL}/{topic}')
 
 
 if __name__ == '__main__':
