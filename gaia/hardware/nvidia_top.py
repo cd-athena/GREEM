127396f8e9dab2c8cb78e75c9c7bba437cb5a79b
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-09-13 18:38:34 +0200

    Made small change to init of NvidiaTop

---
+++ gaia/hardware/nvidia_top.py
@@ -0,0 +1,56 @@
+from nvitop import Device, ResourceMetricCollector
+import pandas as pd
+from time import sleep
+
+import os
+
+
+class NvidiaTop():
+
+    def __init__(self):
+        self.cuda_available: bool = True
+        self.device: Device | None = Device.all() if self.cuda_available else None
+        self.resource_metric_collector: ResourceMetricCollector | None = ResourceMetricCollector() if self.cuda_available else None
+
+    def get_resource_metrics_as_dict(self, cmd: str) -> dict[str, float]:
+        metric_dict: [str, float] = dict()
+
+        def cleanup_key(key: str) -> str:
+
+            return key.removeprefix('<tag>/').replace(' ', '').replace('(%)', '').replace('(C)', '').replace('(W)', '').replace('(', '.').replace(')', '').replace('/', '.')
+
+        with self.resource_metric_collector(tag='<tag>') as collector:
+            os.system(cmd)
+            collect_dict = collector.collect()
+
+            metric_dict = {cleanup_key(k): v for k, v in collect_dict.items()}
+
+        return metric_dict
+    
+    def get_resource_metric_as_dataframe(self, cmd: str) -> pd.DataFrame:
+        
+        metric_dict = self.get_resource_metrics_as_dict(cmd=cmd)
+        
+        return pd.DataFrame.from_dict(metric_dict, orient='index').transpose()
+
+
+if __name__ == '__main__':
+
+    nvi_top = NvidiaTop()
+
+    # collect_dict = nvi_top.get_resource_metrics_as_dict()
+
+    # print('\n'.join([f'{k}: {collect_dict[k]}' for k in collect_dict.keys()]))
+    
+    metrics: list[pd.DataFrame] = list()
+    for i in range(3):
+        
+        metrics.append(nvi_top.get_resource_metric_as_dataframe(cmd='sleep 2'))
+        
+        
+    concat = pd.concat(metrics, ignore_index=True)
+    
+    print(concat)
+    concat.to_csv('test.csv')
+
+ 
