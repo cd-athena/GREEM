2fd320bddeaf3df5ed9e4965adf80765df4583c9
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-09-26 13:30:49 +0200

    Continued working on decoding benchmark

--- gaia/benchmark/decoding/segment_decoding.py
+++ gaia/benchmark/decoding/segment_decoding.py
@@ -17,18 +17,31 @@ from gaia.hardware.nvidia_top import NvidiaTop
 
 from gaia.utils.benchmark import CLI_PARSER
 
+ENCODING_CONFIG_PATHS: list[str] = [
+    '../config_files/test_decoding_config.yaml',
+]
+
 NTFY_TOPIC: str = 'decoding'
+INPUT_FILE_DIR: str = '../result'
+RESULT_ROOT = 'decoding_results'
 
 DRY_RUN: bool = CLI_PARSER.is_dry_run()
 USE_CUDA: bool = CLI_PARSER.is_cuda_enabled()
 INCLUDE_CODE_CARBON: bool = CLI_PARSER.is_code_carbon_enabled()
 
+
+
 def get_input_files(decoding_config: DecodingConfig) -> list[str]:
     input_files: list[str] = list()
-    
+
     # TODO
     
     return input_files
+
+
+def execute_decoding_benchmark():
+    # TODO
+    pass
 
 
 if __name__ == '__main__':
@@ -38,6 +51,22 @@ if __name__ == '__main__':
               - CUDA: {USE_CUDA} 
               - DRY_RUN: {DRY_RUN}
               ''')
+        
+        Path(RESULT_ROOT).mkdir(parents=True, exist_ok=True)
+        
+        gpu_monitoring = None
+        if USE_CUDA:
+            nvidia_top = NvidiaTop()
+            metric_results: list[pd.DataFrame] = list()
+            
+        intel_rapl_workaround()
+        IdleTimeEnergyMeasurement.measure_idle_energy_consumption(result_path=f'{RESULT_ROOT}/decoding_idle_time.csv', idle_time_in_seconds=1)
+
+        decoding_configs: list[DecodingConfig] = [DecodingConfig.from_file(file_path) for file_path in DECODING_CONFIG_PATHS]
+        
+        execute_decoding_benchmark()
+
+
     except Exception as err:
         print('err', err)
         send_ntfy(
