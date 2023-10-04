7c10b017fe466f240d8f754cede03a89f3f50c8d
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-10-04 11:14:46 +0200

    updated ntfy package

--- gaia/utils/ntfy.py
+++ gaia/utils/ntfy.py
@@ -6,9 +6,11 @@ USE_NTFY: bool = CLI_PARSER.is_ntfy_enabled()
 
 NTFY_BASE_URL: str = '3.76.6.7'
 
-def send_ntfy(topic: str, message: str) -> str:
+def send_ntfy(topic: str, message: str, print_message: bool = False) -> str:
     if USE_NTFY:
         os.system(f'curl -d "{message}" {NTFY_BASE_URL}/{topic}')
+    if print_message:
+        print(message)
 
 
 if __name__ == '__main__':
