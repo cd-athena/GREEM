85393c74c2c1d1ae3051f79590ba998b5e28c462
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-10-13 10:27:21 +0200

    Added content to segment_decoding benchmark

--- gaia/benchmark/decoding/segment_decoding.py
+++ gaia/benchmark/decoding/segment_decoding.py
@@ -15,7 +15,6 @@ from gaia.utils.dataframe import get_dataframe_from_csv
 from gaia.utils.ntfy import send_ntfy
 
 # from gaia.hardware.intel import intel_rapl_workaround
-# from gaia.hardware.nvidia_top import NvidiaTop
 
 from gaia.utils.benchmark import CLI_PARSER
 
@@ -33,6 +32,10 @@ RESULT_ROOT = 'decoding_results'
 DRY_RUN: bool = CLI_PARSER.is_dry_run()
 USE_CUDA: bool = CLI_PARSER.is_cuda_enabled()
 INCLUDE_CODE_CARBON: bool = CLI_PARSER.is_code_carbon_enabled()
+IS_QUIET: bool = CLI_PARSER.is_quiet_ffmpeg()
+
+if USE_CUDA:
+    from gaia.hardware.nvidia_top import NvidiaTop
 
 def prepare_data_directories(
     decoding_config: DecodingConfig,
@@ -48,6 +51,45 @@ def prepare_data_directories(
     
     for directory in data_directories:
         Path(directory).mkdir(parents=True, exist_ok=True)
+        
+def get_video_name_from_path(video_path: str) -> str:
+    path_elems: list[str] = video_path.split('/')
+    # TODO find better way than to hardcode the video_name
+    return path_elems[4]
+
+
+def start_demuxing(input_file_path: str, output_path: str) -> None:
+    demuxed_output_path: str = f'{output_path}/demuxed.mp4'
+    demuxing_cmd: str = f'ffmpeg -y -i {input_file_path} {demuxed_output_path}'
+    
+    if USE_CUDA:
+        pass
+    else:
+        os.system(demuxing_cmd)
+        
+def start_decoding(input_file_path: str, output_path: str) -> None:
+    decoding_output_path: str = f'{output_path}/decoding.yuv'
+    decoding_cmd: str = f'ffmpeg -y -i {input_file_path} {decoding_output_path}'
+    
+    if USE_CUDA:
+        pass
+    else:
+        os.system(decoding_cmd)
+        
+
+def start_scaling(output_path: str, dto: DecodingConfigDTO) -> None:
+    input_file_path: str = f'{output_path}/decoding.yuv'
+    scaling_output_path: str = f'{output_path}/scaling.yuv'
+    scaling_cmd: str = f'ffmpeg {IS_QUIET} -f rawvideo -vcodec rawvideo -s ' \
+                        f'{dto.encoding_rendition.width}x{dto.encoding_rendition.height} -r 23.98 ' \
+                        f'-pix_fmt yuv420p -i {input_file_path} ' \
+                        f'-vf scale={dto.scaling_resolution.get_resolution_dir_representation()} ' \
+                        f'-y {scaling_output_path}'
+    
+    if USE_CUDA:
+        pass
+    else:
+        os.system(scaling_cmd)    
 
 
 def execute_decoding_benchmark():
@@ -62,79 +104,44 @@ def execute_decoding_benchmark():
             
             for encoded_file_path in encoded_input_files:
                 
-                demuxed_output_path: str = encoded_file_path.replace("output", "demuxed")
-                decoding_output_path: str = encoded_file_path.replace("output.mp4", "decoded.yuv")
-                scale_output_path: str = encoded_file_path.replace(
-                    "output", f"scale_{dto.scaling_resolution.width}x{dto.scaling_resolution.height}.yuv")
+                video_name = get_video_name_from_path(encoded_file_path)
+                output_path: str = dto.get_output_dir(RESULT_ROOT, video_name)
+                Path(output_path).mkdir(parents=True, exist_ok=True)
                 
-                demuxing_cmd: str = f'ffmpeg -y -i {encoded_file_path} {demuxed_output_path}'
-                decoding_cmd: str = f'ffmpeg -y -i {encoded_file_path} {decoding_output_path}'
-                scale_cmd: str = f'ffmpeg -v quiet -f rawvideo -vcodec rawvideo -s ' \
-                        f'{dto.encoding_rendition.width}x{dto.encoding_rendition.height} -r 23.98 ' \
-                        f'-pix_fmt yuv420p -i {decoding_output_path} ' \
-                        f'-vf scale={dto.scaling_resolution.width}x{dto.scaling_resolution.height} -y {scale_output_path}'
-                
-                os.system(demuxing_cmd)
-                os.system(decoding_cmd)
-                os.system(scale_cmd)
-                
-                os.system(f'rm {demuxed_output_path} {decoding_output_path} {scale_output_path}')
-            pass
-    pass
+                start_demuxing(encoded_file_path, output_path)
+                start_decoding(encoded_file_path, output_path)
+                start_scaling(output_path, dto)
+    
+    # TODO store benchmark results
 
 
 if __name__ == '__main__':
-    # decoding_configs: list[DecodingConfig] = [DecodingConfig.from_file(file_path) for file_path in DECODING_CONFIG_PATHS]
-    # all_video_files = get_all_possible_video_files()
-    # config = decoding_configs[0]
+        
+    try:
+        send_ntfy(NTFY_TOPIC,
+                  f'''start decoding benchmark 
+              - CUDA: {USE_CUDA} 
+              - DRY_RUN: {DRY_RUN}
+              ''')
     
-    # print(all_video_files)
-    
-    execute_decoding_benchmark()
-    
-    # times: list = list()
-    
-    # for _ in range(10):
-    #     start = time()  
-    #     for dto in config.get_decoding_dtos():
-    #         # print(dto.encoding_preset, dto.encoding_codec, dto.encoding_rendition, dto.framerate)
-    #         input_files = get_input_files(dto, all_video_files)
-    #         # print(input_files[:10])
-
-    #     times.append(time() - start)
-        
-    # print(sum(times) / len(times))
-        
-        # test
-        
-    # try:
-    #     send_ntfy(NTFY_TOPIC,
-    #               f'''start decoding benchmark 
-    #           - CUDA: {USE_CUDA} 
-    #           - DRY_RUN: {DRY_RUN}
-    #           ''')
-        
-    #     Path(RESULT_ROOT).mkdir(parents=True, exist_ok=True)
-        
-    #     gpu_monitoring = None
-    #     if USE_CUDA:
-    #         nvidia_top = NvidiaTop()
-    #         metric_results: list[pd.DataFrame] = list()
+    # TODO add monitoring
+        if USE_CUDA:
+            nvidia_top = NvidiaTop()
+            metric_results: list[pd.DataFrame] = list()
             
     #     intel_rapl_workaround()
     #     IdleTimeEnergyMeasurement.measure_idle_energy_consumption(result_path=f'{RESULT_ROOT}/decoding_idle_time.csv', idle_time_in_seconds=1)
 
-    #     decoding_configs: list[DecodingConfig] = [DecodingConfig.from_file(file_path) for file_path in DECODING_CONFIG_PATHS]
-        
-    #     execute_decoding_benchmark()
+        execute_decoding_benchmark()
 
 
-    # except Exception as err:
-    #     print('err', err)
-    #     send_ntfy(
-    #         NTFY_TOPIC, f'Something went wrong during the decoding benchmark, Exception: {err}')
+    except Exception as err:
+        print('err', err)
+        send_ntfy(
+            NTFY_TOPIC, 
+            f'Something went wrong during the decoding benchmark, Exception: {err}', 
+            print_message=True)
 
-    # finally:
-    #     print('done')
-    #     send_ntfy(NTFY_TOPIC, 'finished decoding benchmark')
+    finally:
+        send_ntfy(NTFY_TOPIC, 'finished decoding benchmark', print_message=True)
     
