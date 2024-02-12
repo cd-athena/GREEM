f766cdc3a373194d681fb00176cdcf1ac2ae462d
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-02-12 17:53:53 +0100

    Added utility file called video processing

---
+++ gaia/utils/video_processing.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+
+class Codecs(str, Enum):
+    """Enum class providing supported video processing codecs.
+
+    Returns
+    -------
+    Codecs Enum
+        _description_
+        
+    Functions
+    ---------
+    
+    get_lib_codec(self, cuda_mode) -> str
+        returns the FFmpeg library as string of the instance
+
+
+    """
+    H264 = 'h264'
+    H265 = 'h265'
+    AVC = 'h264'
+    HEVC = 'h265'
+    AV1 = 'av1'
+    VP9 = 'vp9'
+    VVC = 'vvc'
+
+    def get_lib_codec(self, cuda_mode: bool = False) -> str:
+        '''Returns the codec for the ffmpeg command'''
+        if self.value == 'h264':
+            return 'h264_nvenc' if cuda_mode else 'libx264'
+        if self.value == 'h265':
+            return 'hevc_nvenc' if cuda_mode else 'libx265'
+        if self.value == 'av1':
+            return 'libsvtav1'
+        if self.value == 'vp9':
+            return 'libvpx-vp9'
+        if self.value == 'vvc':
+            return 'vvc'
+            
+        return ''
+    
