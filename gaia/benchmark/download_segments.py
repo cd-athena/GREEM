d2e7a1132ea506cb78fa8d0b771de115db099cf8
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-02-26 12:46:15 +0100

    just a small video set for test download

--- gaia/benchmark/download_segments.py
+++ gaia/benchmark/download_segments.py
@@ -3,21 +3,24 @@ import time
 from multiprocessing import cpu_count
 from multiprocessing.pool import ThreadPool
 
-all_segments = [("AncientThought_s0{}", 0, 36), ("Basketball_s0{}", 0, 1), ("Beauty_s0{}", 0, 0),
-                ("Bosohorus_s0{}", 0, 0), ("BundNightScape_s0{}", 0, 1), ("Bunny_s0{}", 0, 1),
-                ("Busy_City_s0{}", 0, 24), ("CampfireParty_s0{}", 0, 1), ("Characters_s0{}", 0, 1),
-                ("Childs_Play_s0{}", 0, 18), ("CityAlley_s0{}", 0, 1), ("ConstructionField_s0{}", 0, 1),
-                ("Construction_s0{}", 0, 1), ("Crowd_s0{}", 0, 1), ("Dolls_s0{}", 0, 1), ("Eldorado_s0{}", 0, 35),
-                ("FlowerFocus_s0{}", 0, 1), ("FlowerKids_s0{}", 0, 1), ("Flowers_s0{}", 0, 1),
-                ("Fountains_s0{}", 0, 1), ("Fun_on_the_river_s0{}", 0, 28), ("HoneyBee_s0{}", 0, 0),
-                ("Indoor_Soccer_s0{}", 0, 49), ("Jockey_s0{}", 0, 0), ("Lake_s0{}", 0, 1), ("Library_s0{}", 0, 1),
-                ("Lifting_Off_s0{}", 0, 39), ("Maples_s0{}", 0, 1), ("Marathon_s0{}", 0, 1),
-                ("Nature_in_the_city_s0{}", 0, 28), ("Park_s0{}", 0, 1), ("RaceNight_s0{}", 0, 1),
-                ("ReadySetGo_s0{}", 0, 0), ("ResidentialBuilding_s0{}", 0, 1), ("RiverBank_s0{}", 0, 1),
-                ("Runners_s0{}", 0, 1), ("RushHour_s0{}", 0, 1), ("Scarf_s0{}", 0, 0),
-                ("Secounds_That_Count_s0{}", 0, 63), ("Skateboarding_s0{}", 0, 54), ("TallBuildings_s0{}", 0, 1),
-                ("TrafficAndBuilding_s0{}", 0, 1), ("TrafficFlow_s0{}", 0, 1), ("TreeShade_s0{}", 0, 1),
-                ("Unspoken_Friend_s0{}", 0, 50), ("Wood_s0{}", 0, 1), ("YachtRide_s0{}", 0, 0)]
+all_segments = [
+    # ("AncientThought_s0{}", 0, 36),
+    ("Basketball_s0{}", 0, 1), ("Beauty_s0{}", 0, 0),
+    ("Bosohorus_s0{}", 0, 0), ("BundNightScape_s0{}", 0, 1), ("Bunny_s0{}", 0, 1),
+    # ("Busy_City_s0{}", 0, 24), ("CampfireParty_s0{}", 0, 1), ("Characters_s0{}", 0, 1),
+    # ("Childs_Play_s0{}", 0, 18), ("CityAlley_s0{}", 0, 1), ("ConstructionField_s0{}", 0, 1),
+    # ("Construction_s0{}", 0, 1), ("Crowd_s0{}", 0, 1), ("Dolls_s0{}", 0, 1), ("Eldorado_s0{}", 0, 35),
+    # ("FlowerFocus_s0{}", 0, 1), ("FlowerKids_s0{}", 0, 1), ("Flowers_s0{}", 0, 1),
+    # ("Fountains_s0{}", 0, 1), ("Fun_on_the_river_s0{}", 0, 28), ("HoneyBee_s0{}", 0, 0),
+    # ("Indoor_Soccer_s0{}", 0, 49), ("Jockey_s0{}", 0, 0), ("Lake_s0{}", 0, 1), ("Library_s0{}", 0, 1),
+    # ("Lifting_Off_s0{}", 0, 39), ("Maples_s0{}", 0, 1), ("Marathon_s0{}", 0, 1),
+    # ("Nature_in_the_city_s0{}", 0, 28), ("Park_s0{}", 0, 1), ("RaceNight_s0{}", 0, 1),
+    # ("ReadySetGo_s0{}", 0, 0), ("ResidentialBuilding_s0{}", 0, 1), ("RiverBank_s0{}", 0, 1),
+    # ("Runners_s0{}", 0, 1), ("RushHour_s0{}", 0, 1), ("Scarf_s0{}", 0, 0),
+    # ("Secounds_That_Count_s0{}", 0, 63), ("Skateboarding_s0{}", 0, 54), ("TallBuildings_s0{}", 0, 1),
+    # ("TrafficAndBuilding_s0{}", 0, 1), ("TrafficFlow_s0{}", 0, 1), ("TreeShade_s0{}", 0, 1),
+    # ("Unspoken_Friend_s0{}", 0, 50), ("Wood_s0{}", 0, 1), ("YachtRide_s0{}", 0, 0)
+]
 
 video_path_list: list[str] = list()
 for j in range(len(all_segments)):
@@ -35,7 +38,8 @@ for j in range(len(all_segments)):
 
 video_input_outputs: list[tuple[str, str]] = [
     (video, 'dataset/ref_265') for video in video_path_list]
-        
+
+
 def download_url(args):
     start_time = time.time()
     print(args)
