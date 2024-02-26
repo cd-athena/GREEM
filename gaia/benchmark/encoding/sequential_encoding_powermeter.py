da4232242262a10a0ec8398023e8ee437664bb47
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-02-26 12:35:16 +0100

    prepared powermeter test

--- gaia/benchmark/encoding/sequential_encoding_powermeter.py
+++ gaia/benchmark/encoding/sequential_encoding_powermeter.py
@@ -37,185 +37,6 @@ DRY_RUN: bool = CLI_PARSER.is_dry_run()
 USE_CUDA: bool = CLI_PARSER.is_cuda_enabled()
 INCLUDE_CODE_CARBON: bool = CLI_PARSER.is_code_carbon_enabled()
 
-"""
-def encoding(input_ffmpeg: str,
-             output_file: str,
-             codec: str,
-             encoding_preset: str,
-             bitrate: str,
-             width: str,
-             height: str
-             ) -> str:
-    output_file_name: str = build_output_file_name(
-        output_file, codec, encoding_preset, bitrate, width, height)
-
-    result_file_path: str = f'{RESULT_PATH}/{codec}/enc/{output_file_name}.{codec}'
-
-    lib_params: str = ' -x264-params' if '264' in codec else ' -x265-params'
-
-    command = f'ffmpeg {FFMPEG_QUIET} -y {FFMPEG_CUDA} -i {input_ffmpeg}'\
-        f' -probesize 10M -vcodec {get_codec_lib(codec)}'\
-        f'{lib_params} "log-level=error --keyint {IFRAME_INTERVAL*FPS} --min-keyint {IFRAME_INTERVAL*FPS} --no-scenecut" -preset {encoding_preset}' \
-        f' -b:v {bitrate}k -minrate {bitrate}k -maxrate {bitrate}k -bufsize {3*int(bitrate)}k' \
-        f' {result_file_path}'
-
-    os.system(command)
-    return result_file_path
-
-"""
-
-
-# def create_ffmpeg_encoding_command(
-#     input_file_path: str,
-#     output_dir: str,
-#     rendition: Rendition,
-#     preset: str,
-#     segment_duration: int,
-#     codec: str,
-#     framerate: int = 0,
-#     constant_rate_factor: int = -1,
-#     cuda_enabled: bool = False,
-#     quiet_mode: bool = False,
-# ) -> str:
-#     """Creates the ffmpeg command for encoding a video file
-
-#         command = f'ffmpeg {FFMPEG_QUIET} -y {FFMPEG_CUDA} -i {input_ffmpeg}'\
-#         f' -probesize 10M -vcodec {get_codec_lib(codec)}'\
-#         f'{lib_params} "log-level=error --keyint {IFRAME_INTERVAL*FPS} --min-keyint {IFRAME_INTERVAL*FPS} --no-scenecut" -preset {encoding_preset}' \
-#         f' -b:v {bitrate}k -minrate {bitrate}k -maxrate {bitrate}k -bufsize {3*int(bitrate)}k' \
-#         f' {result_file_path}'
-#         """
-#     cmd: list[str] = ["ffmpeg -y"]
-#     if cuda_enabled:
-#         cmd.append(CUDA_ENC_FLAG)
-#     if quiet_mode:
-#         cmd.append(QUIET_FLAG)
-
-#     cmd.append(f"-re -i {input_file_path}")
-
-#     if constant_rate_factor > -1:
-#         cmd.append(f"-crf {constant_rate_factor}")
-
-#     fps_str: str = ""
-#     if framerate > 0:
-#         fps_str = str(framerate)  # type: ignore
-
-#     cmd.extend(get_representation_ffmpeg_flags(
-#         [rendition], preset, codec, fps=fps_str))
-
-#     fps: int = (
-#         ceil(VideoInfo(input_file_path).get_fps())
-#         if framerate is None or framerate == 0
-#         else framerate
-#     )
-#     keyframe: int = fps * segment_duration
-
-#     cmd.extend(
-#         [
-#             f"-keyint_min {keyframe}",
-#             f"-g {keyframe}",
-#         ]
-#     )
-
-#     cmd.extend([f"{output_dir}/output.mp4"])
-
-#     join_string: str = " \n" if DRY_RUN else " "
-
-#     lib_params: str = " -x264-params" if "264" in codec else " -x265-params"
-
-#     command = (
-#         f"ffmpeg {QUIET_FLAG} -y {CUDA_ENC_FLAG} -i {input_file_path}"
-#         f" -probesize 10M -vcodec {get_lib_codec(codec)}"
-#         f'{lib_params} "log-level=error --keyint {keyframe} --min-keyint {keyframe} --no-scenecut" -preset {preset}'
-#         f" -b:v {rendition.bitrate}k -minrate {rendition.bitrate}k -maxrate {rendition.bitrate}k -bufsize {3*int(rendition.bitrate)}k"
-#         f" {output_dir}/encoding_output.mp4"
-#     )
-
-#     return command
-# return join_string.join(cmd)
-
-
-"""
-def scaling(
-        input_ffmpeg: str, 
-        output_file: str, 
-        codec: str, 
-        encoding_preset: str, 
-        bitrate: str,
-        width: str, 
-        height: str
-        ):
-    output_file_name = build_output_file_name(
-        output_file, codec, encoding_preset, bitrate, width, height)
-
-    result_file_path: str = f'{RESULT_PATH}/{codec}/enc/{output_file_name}_scaled.{codec}'
-
-    command = f'ffmpeg {FFMPEG_QUIET} -y {FFMPEG_CUDA} -i {input_ffmpeg} '\
-        f'-vf scale={width}:{height} ' \
-        f'{result_file_path}'
-
-    os.system(command)
-    return result_file_path"""
-
-
-# def create_ffmpeg_scaling_command(
-#     output_dir: str,
-#     rendition: Rendition,
-#     cuda_enabled: bool = False,
-#     quiet_mode: bool = False,
-# ) -> str:
-#     cmd: list[str] = ["ffmpeg -y"]
-#     if cuda_enabled:
-#         cmd.append(CUDA_ENC_FLAG)
-#     if quiet_mode:
-#         cmd.append(QUIET_FLAG)
-
-#     cmd.append(f"-re -i {output_dir}/encoding_output.mp4")
-
-#     cmd.extend(
-#         [
-#             f"-vf scale={rendition.get_resolution_dir_representation()}",
-#             f"{output_dir}/scaling_output.mp4",
-#         ]
-#     )
-
-#     # command = f'ffmpeg {QUIET_FLAG} -y {CUDA_ENC_FLAG} -i {input_file_path} '\
-#     #     f'-vf scale={rendition.width}:{rendition.height} ' \
-#     #     f'{output_dir}/output.mp4'
-
-#     join_string: str = " \n" if DRY_RUN else " "
-
-#     # return command
-#     return join_string.join(cmd)
-
-
-# def get_representation_ffmpeg_flags(
-#     renditions: list[Rendition],
-#     preset: str,
-#     codec: str,
-#     fps: str = "",
-# ) -> list[str]:
-#     """Returns the ffmpeg flags for the renditions"""
-#     representations: list[str] = list()
-
-#     fps_repr: str = "" if len(fps) == 0 else f",fps={fps}"
-
-#     for idx, rendition in enumerate(renditions):
-#         bitrate = rendition.bitrate
-#         height = rendition.height
-#         width = rendition.width
-#         representation: list[str] = [
-#             f"-b:v:{idx} {bitrate}k -minrate {bitrate}k -maxrate {bitrate}k -bufsize {3*int(bitrate)}k",
-#             f"-c:v:{idx} {get_lib_codec(codec)} -filter:v:{idx}",
-#             f'"scale={width}:{height}',
-#             f'{fps_repr}"',
-#             f"-preset {preset}",
-#         ]
-
-#         representations.extend(representation)
-
-#     return representations
-
 
 def prepare_data_directories(
     encoding_config: EncodingConfig,
@@ -279,11 +100,14 @@ def get_filtered_sliced_videos(
 def execute_encoding_benchmark(encoding_configs: list[EncodingConfig]):
 
     input_dir = INPUT_FILE_DIR
-
+    time_dir = {
+        'start': datetime.now().__str__()
+    }
+    start_time = datetime.now()
     for encoding_config in encoding_configs:
         input_files = sorted(
             [file for file in os.listdir(
-                INPUT_FILE_DIR) if file.endswith(".265")]
+                INPUT_FILE_DIR) if file.endswith(".265")][:3]
         )
 
         # encode for each duration defined in the config file
@@ -294,39 +118,26 @@ def execute_encoding_benchmark(encoding_configs: list[EncodingConfig]):
         # encode each video found in the input files corresponding to the duration
         for video_name in input_files:
             for dto in encoding_dtos:
-                # output_dir = f'{RESULT_ROOT}/{dto.get_output_directory(video_name.removesuffix(".265"))}'
+
                 input_file_path = f'{input_dir}/{video_name}'
+
+                time_dir[f'{video_name}_start'] = datetime.now().__str__()
+
                 encoding_cmd = (
-                    # create_ffmpeg_encoding_command(
-                    #     f"{input_dir}/{video_name}",
-                    #     output_dir,
-                    #     dto.rendition,
-                    #     dto.preset,
-                    #     duration,
-                    #     dto.codec,
-                    #     framerate=dto.framerate,
-                    #     constant_rate_factor=-1,
-                    #     cuda_enabled=USE_CUDA,
-                    #     quiet_mode=CLI_PARSER.is_quiet_ffmpeg(),
-                    # )
                     create_sequential_encoding_cmd(
                         input_file_path, video_name, RESULT_ROOT, dto)
                     if not DRY_RUN
                     else "sleep 0.1"
                 )
 
-                print(encoding_cmd)
                 execute_encoding_stage(encoding_cmd, dto, video_name)
+                time_dir[f'{video_name}_end'] = datetime.now().__str__()
                 break
 
-                # scaling_cmd: str = create_ffmpeg_scaling_command(
-                #     output_dir, dto.rendition, cuda_enabled=USE_CUDA
-                # )
-
-                # execute_encoding_cmd(cmd, dto, video_name)
-                # execute_scaling_stage(scaling_cmd, dto, video_name)
-            break
-    # write_encoding_results_to_csv()
+    time_dir['end'] = datetime.now().__str__()
+    print(time_dir)
+    pd.Series(time_dir).to_csv(f'start_stop_times_{datetime.now()}.csv')
+    write_encoding_results_to_csv()
 
 
 @track_emissions(
@@ -344,29 +155,6 @@ def execute_encoding_stage(
     execute_encoding_cmd(cmd, encoding_dto, video_name)
 
 
-@track_emissions(
-    offline=True,
-    country_iso_code="AUT",
-    log_level="error" if CLI_PARSER.is_quiet_ffmpeg() else "debug",
-    measure_power_secs=1,
-    output_dir=RESULT_ROOT,
-    save_to_file=True,
-    project_name="scaling_stage",
-)  # type: ignore
-def execute_scaling_stage(
-    cmd: str, encoding_dto: EncodingConfigDTO, video_name: str
-) -> None:
-    execute_scaling_stage(cmd, encoding_dto, video_name)
-
-
-@track_emissions(
-    offline=True,
-    country_iso_code="AUT",
-    log_level="error" if CLI_PARSER.is_quiet_ffmpeg() else "debug",
-    measure_power_secs=1,
-    output_dir=RESULT_ROOT,
-    save_to_file=True,
-)  # type: ignore
 def execute_encoding_cmd(
     cmd: str, encoding_dto: EncodingConfigDTO, video_name: str
 ) -> None:
