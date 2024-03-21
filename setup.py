b163fa68fb1eba6e6332c1215f7204dedc894be9
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-03-21 14:57:01 +0100

    Started renaming repository structure to fit paper

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
 
-setup(name='gaia', version='0.1', packages=find_packages())
+setup(name='greem', version='0.1', packages=find_packages())
 
 # TODO create folders for project
