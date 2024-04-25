3de8548515370ef4f7b08253bfe7e38de6072f67
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-04-25 17:45:49 +0200

    Working on MetricTracker class that includes CodeCarbon and nvitop measurements

--- greem/utility/monitoring.py
+++ greem/utility/monitoring.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from codecarbon import OfflineEmissionsTracker
+from codecarbon.emissions_tracker import BaseEmissionsTracker
 from codecarbon.output import EmissionsData
 from codecarbon.external.scheduler import PeriodicScheduler
 from pydantic import BaseModel
@@ -7,8 +10,13 @@ from abc import ABC, abstractmethod
 from os import system
 from collections import deque
 from dataclasses import dataclass, field
+from nvitop import ResourceMetricCollector
 
 import copy
+
+# stolen from CodeCarbon EmissionsTracker
+_sentinel = object()
+
 
 # from greem.utility.cli_parser import CUDA_ENC_FLAG
 
@@ -37,6 +45,7 @@ class BaseMonitoring(ABC):
     cuda_enabled: bool = False
     collected_data: deque[MonitoringData] = field(default_factory=deque)
     tracker: OfflineEmissionsTracker = None
+    country_iso_code: str = 'AUT'
 
     @abstractmethod
     def start_monitoring(self, cmd: str):
@@ -51,62 +60,120 @@ class BaseMonitoring(ABC):
     def __post_init__(self):
         if self.tracker is None:
             self.tracker = OfflineEmissionsTracker(
-                measure_power_secs=self.measure_power_secs, save_to_file=False, country_iso_code='AUT')
-        self.tracker.start()
+                measure_power_secs=self.measure_power_secs,
+                save_to_file=False,
+                country_iso_code=self.country_iso_code)
+        if self.cuda_enabled:
+            pass
+
+        # self.tracker.start()
+
+    def flush_monitoring_data(self, delta: bool = True) -> MonitoringData:
+        codecarbon_data = self.tracker._prepare_emissions_data(delta=delta)
+        if self.cuda_enabled:
+            # cuda_data =
+            pass
+        return MonitoringData(codecarbon_data)
 
 
 @dataclass
 class RegularMonitoring(BaseMonitoring):
 
     def start_monitoring(self, cmd: str):
+        self.flush_monitoring_data()
 
-        self.tracker._prepare_emissions_data(delta=True)
+        system(cmd)
 
-        if self.cuda_enabled:
-            # TODO
-            pass
-        else:
-            system(cmd)
-
-            monitoring_data: MonitoringData = MonitoringData(self.tracker._prepare_emissions_data(
-                delta=True))
-
-            self.collected_data.append(monitoring_data)
+        monitoring_data: MonitoringData = self.flush_monitoring_data()
+        self.collected_data.append(monitoring_data)
 
 
 @dataclass
 class PeriodicMonitoring(BaseMonitoring):
-
-    scheduler: PeriodicScheduler = None
+    _scheduler: PeriodicScheduler = None
 
     def start_monitoring(self, cmd: str):
-        self.tracker._prepare_emissions_data()
+        self.flush_monitoring_data()  # start a new monitoring cycle
 
-        self.scheduler.start()
-        if self.cuda_enabled:
-            # TODO
-            pass
-        else:
-            system(cmd)
-
-        self.scheduler.stop()
+        self.start()
+        system(cmd)
+        self.stop()
 
     def __post_init__(self):
         super().__post_init__()
-        self.collected_data = []
-        self.scheduler = PeriodicScheduler(
+        self.collected_data = deque()
+        self._scheduler = PeriodicScheduler(
             function=self._fetch_hardware_metrics,
             interval=self.measure_power_secs
         )
 
     def start(self):
-        self.scheduler.start()
+        self._scheduler.start()
 
     def stop(self):
-        self.scheduler.stop()
+        self._scheduler.stop()
 
     def _fetch_hardware_metrics(self):
-        monitoring_data: MonitoringData = MonitoringData(self.tracker._prepare_emissions_data(
-            delta=True))
+        monitoring_data: MonitoringData = self.flush_monitoring_data()
 
+        self.collected_data.append(monitoring_data)
+
+
+class MetricTracker(OfflineEmissionsTracker):
+    collected_data: deque[MonitoringData] = field(default_factory=deque)
+    extended_gpu_metrics: bool = False
+    gpu_collector: ResourceMetricCollector = None
+
+    def __init__(
+            self,
+            *args,
+            country_iso_code: Optional[str] = 'AUT',
+            extended_gpu_metrics: bool = False,
+            measure_power_secs: float = 1,
+            region: Optional[str] = _sentinel,
+            cloud_provider: Optional[str] = _sentinel,
+            cloud_region: Optional[str] = _sentinel,
+            country_2letter_iso_code: Optional[str] = _sentinel,
+            **kwargs,
+    ):
+        super(MetricTracker, self).__init__(
+            country_iso_code=country_iso_code,
+            region=region,
+            cloud_provider=cloud_provider,
+            cloud_region=cloud_region,
+            country_2letter_iso_code=country_2letter_iso_code,
+            *args,
+            **kwargs,
+        )
+
+        self.collected_data = deque()
+        self.measure_power_secs = measure_power_secs
+        # if GPU support is enabled, initialise the nvitop.ResourceMetricCollector
+        self.extended_gpu_metrics = extended_gpu_metrics
+        if self.extended_gpu_metrics:
+            self.gpu_collector = ResourceMetricCollector()
+
+    def __post_init__(self):
+        self._scheduler = PeriodicScheduler(
+            function=self._fetch_hardware_metrics,
+            interval=self.measure_power_secs
+        )
+
+    def flush(self) -> Optional[MonitoringData]:
+        codecarbon_data: EmissionsData = self._tracker._prepare_emissions_data(delta=True)
+        if self.extended_gpu_metrics:
+            gpu_data = self.gpu_collector.collect()
+            self.gpu_collector.clear()
+            codecarbon_data.__dict__.update(gpu_data)
+
+        return MonitoringData(codecarbon_data)
+
+    def start_monitoring(self, cmd: str, tag: str = ''):
+        pass
+
+    def stop_monitoring(self, tag: str = ''):
+        pass
+
+    def _fetch_hardware_metrics(self):
+        monitoring_data: MonitoringData = self.flush()
         self.collected_data.append(monitoring_data)
