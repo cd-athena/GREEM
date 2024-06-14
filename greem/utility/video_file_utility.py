aefa7a9003ab4c3969e7971e7a969d4c134cc355
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-06-14 15:43:26 +0200

    Added video_file utility class

---
+++ greem/utility/video_file_utility.py
@@ -0,0 +1,16 @@
+def abbreviate_video_name(video_name: str) -> str:
+    video_name_no_ext: str = video_name.replace('.265', '')
+
+    upper_case: str = ''.join(
+        [
+            f'{c}{video_name_no_ext[video_name_no_ext.find(c) + 1]}'
+            for c in video_name_no_ext if c.isupper()
+        ])
+    numbers: str = ''.join([c for c in video_name_no_ext if c.isnumeric()])
+    abbreviate: str = f'{upper_case}_{numbers}'
+
+    return abbreviate
+
+
+def remove_media_extension(file_name: str) -> str:
+    return file_name.removesuffix('.265').removesuffix('.webm').removesuffix('.mp4')
