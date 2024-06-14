96eaebd1a2a62829f359f06887f486fc92f0ff04
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-06-14 15:43:09 +0200

    Changed deque to list in monitoring.py because of nasty race-conditions I don't want to deal with

--- greem/utility/monitoring.py
+++ greem/utility/monitoring.py
@@ -1,7 +1,6 @@
 from os import system
 import copy
 from abc import ABC, abstractmethod
-from collections import deque
 from dataclasses import dataclass, field
 from typing import OrderedDict, Type
 
@@ -16,9 +15,6 @@ import pandas as pd
 @dataclass
 class NviTopData():
     nvitop_dict: dict[str, float]
-    
-    # def __init__(self, nvitop_dict: dict[str, float]) -> None:
-    #     self._nvitop_dict: OrderedDict[str, float] = OrderedDict(nvitop_dict.items())
         
     @property
     def values(self) -> OrderedDict:
@@ -28,26 +24,6 @@ class NviTopData():
         self.nvitop_dict.update(data)
         
 
-# class MonitoringData(EmissionsData):
-#     """Container that contains data fetched from hardware resources.
-
-#     Contains CodeCarbon EmissionsData, but also additional measurements from nvitop.
-#     """
-
-#     def __init__(self, other):
-#         super(EmissionsData, self).__init__()
-
-#     def __new__(cls, other) -> Type['MonitoringData']:
-#         if isinstance(other, EmissionsData):
-#             other = copy.copy(other)
-#             other.__class__ = MonitoringData
-#             return MonitoringData(other)
-#         return object.__new__(cls)
-
-#     def as_dict(self) -> OrderedDict:
-#         return self.values
-
-
 @dataclass  # has to be a dataclass instead of BaseModel because
 # __post_init__ is required
 class BaseMonitoring(ABC):
@@ -55,8 +31,8 @@ class BaseMonitoring(ABC):
     cuda_enabled: bool = False
     tracker: OfflineEmissionsTracker = None
     country_iso_code: str = 'AUT'
-    collected_codecarbon_data: deque[EmissionsData] = field(default_factory=deque)
-    collected_nvitop_data: deque = field(default_factory=deque)
+    collected_codecarbon_data: list[EmissionsData] = field(default_factory=list)
+    collected_nvitop_data: list = field(default_factory=list)
     gpu_collector: ResourceMetricCollector = None
 
     @abstractmethod
@@ -81,25 +57,10 @@ class BaseMonitoring(ABC):
         codecarbon_data = self.tracker._prepare_emissions_data(delta=delta)
         if self.cuda_enabled:
             gpu_data = self.gpu_collector.collect()
-            # codecarbon_data.__dict__.update(gpu_data)
             self.gpu_collector.clear()
             return codecarbon_data, NviTopData(gpu_data)
         
         return codecarbon_data, NviTopData({})
-        # self.collected_data.append(codecarbon_data)
-        # return MonitoringData(other=codecarbon_data)``
-
-
-# @dataclass
-# class RegularMonitoring(BaseMonitoring):
-
-#     def monitor_process(self, cmd: str):
-#         self.flush_monitoring_data()
-
-#         system(cmd)
-
-#         monitoring_data: MonitoringData = self.flush_monitoring_data()
-#         self.collected_data.append(monitoring_data)
 
 
 @dataclass
@@ -118,11 +79,8 @@ class HardwareTracker(BaseMonitoring):
 
     def __post_init__(self) -> None:
         super().__post_init__()
-        # self.collected_data = deque()
-        self.collected_codecarbon_data = deque()
-        self.collected_nvitop_data = deque()
-        # if self.cuda_enabled:
-            # self.gpu_collector = 
+        self.collected_codecarbon_data = []
+        self.collected_nvitop_data = []
         self._scheduler = PeriodicScheduler(
             function=self._fetch_hardware_metrics,
             interval=self.measure_power_secs
@@ -142,7 +100,6 @@ class HardwareTracker(BaseMonitoring):
     def stop(self) -> None:
         """Stop the monitoring libraries
         """
-        # self._fetch_hardware_metrics()
         self._scheduler.stop()
         self.tracker.stop()
         if self.cuda_enabled:
@@ -151,7 +108,6 @@ class HardwareTracker(BaseMonitoring):
     def clear(self) -> None:
         """Clears the collected data and monitored value
         """
-        # self.collected_data.clear()
         self.collected_codecarbon_data.clear()
         self.collected_nvitop_data.clear()
         self.flush_monitoring_data(delta=True)
@@ -159,7 +115,6 @@ class HardwareTracker(BaseMonitoring):
     def _fetch_hardware_metrics(self) -> None:
         emissions_data, nvitop_data = self.flush_monitoring_data(
             delta=True)
-        # self.collected_data.append(monitoring_data)
         self.collected_codecarbon_data.append(emissions_data)
         self.collected_nvitop_data.append(nvitop_data)
 
