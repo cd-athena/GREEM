621d85387ad0b32beb6f36bc313ddc812229ba6e
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2024-07-22 12:24:24 +0200

    Formatted parallel_encoding with ruff linter

--- greem/testbeds/encoding/parallel_encoding/parallel_encoding.py
+++ greem/testbeds/encoding/parallel_encoding/parallel_encoding.py
@@ -1,33 +1,35 @@
 import os
 from datetime import datetime
-from pathlib import Path
 from enum import Enum
+from pathlib import Path
 
 import pandas as pd
 
-from greem.utility.ffmpeg import (
-    create_one_video_multiple_representation_command, 
-    create_multi_video_ffmpeg_command
-    )
-from greem.utility.configuration_classes import EncodingConfig, EncodingConfigDTO
-
 from greem.utility.cli_parser import CLI_PARSER
+from greem.utility.configuration_classes import EncodingConfig, EncodingConfigDTO
+from greem.utility.ffmpeg import (
+    create_multi_video_ffmpeg_command,
+    create_one_video_multiple_representation_command,
+)
 from greem.utility.monitoring import HardwareTracker
-from greem.utility.video_file_utility import abbreviate_video_name, remove_media_extension
+from greem.utility.video_file_utility import (
+    abbreviate_video_name,
+    remove_media_extension,
+)
 
-NTFY_TOPIC: str = 'gpu_encoding'
+NTFY_TOPIC: str = "gpu_encoding"
 
 ENCODING_CONFIG_PATHS: list[str] = [
-    'config_files/parallel_encoding_h264.yaml',
-    'config_files/parallel_encoding_h265.yaml',
+    "config_files/parallel_encoding_h264.yaml",
+    "config_files/parallel_encoding_h265.yaml",
 ]
 
 # INPUT_FILE_DIR: str = '/home/shared2/athena2/Dataset/Inter4K'
 # INPUT_FILE_DIR: str = '../../dataset/Inter4K/60fps/UHD'
-INPUT_FILE_DIR: str = '../../dataset/Inter4K/60fps/HEVC'
+INPUT_FILE_DIR: str = "../../dataset/Inter4K/60fps/HEVC"
 # INPUT_FILE_DIR: str = '../../dataset/ref_265'
-RESULT_ROOT: str = 'results'
-COUNTRY_ISO_CODE: str = 'AUT'
+RESULT_ROOT: str = "results"
+COUNTRY_ISO_CODE: str = "AUT"
 
 USE_SLICED_VIDEOS: bool = CLI_PARSER.is_sliced_encoding()
 # if True, no encoding will be executed
@@ -41,20 +43,22 @@ CLEANUP: bool = False
 
 TEST_REPETITIONS: int = 1
 
-assert TEST_REPETITIONS > 0, 'must be bigger than zero'
+assert TEST_REPETITIONS > 0, "must be bigger than zero"
 
 GPU_COUNT = 0
 
 if USE_CUDA:
-    from greem.utility.gpu_utils import NvidiaGpuUtils, NvidiaGPUMetadata
+    from greem.utility.gpu_utils import NvidiaGPUMetadata, NvidiaGpuUtils
+
     gpu_utils = NvidiaGpuUtils()
     has_nvidia = gpu_utils.has_nvidia_gpu
     GPU_COUNT = gpu_utils.gpu_count
-    GPU_INFO: list[NvidiaGPUMetadata] = gpu_utils.nvidia_metadata.gpu if has_nvidia else []
+    GPU_INFO: list[NvidiaGPUMetadata] = (
+        gpu_utils.nvidia_metadata.gpu if has_nvidia else []
+    )
     del gpu_utils
 
-hardware_tracker = HardwareTracker(
-    cuda_enabled=USE_CUDA, measure_power_secs=0.5)
+hardware_tracker = HardwareTracker(cuda_enabled=USE_CUDA, measure_power_secs=0.5)
 
 
 class ParallelMode(Enum):
@@ -67,11 +71,9 @@ monitoring_results: list = []
 
 
 def prepare_data_directories(
-        encoding_config: EncodingConfig,
-        result_root: str = RESULT_ROOT,
-        video_names=None
+    encoding_config: EncodingConfig, result_root: str = RESULT_ROOT, video_names=None
 ) -> list[str]:
-    '''Used to generate all directories that are used for the video encoding
+    """Used to generate all directories that are used for the video encoding
 
     Args:
         encoding_config (EncodingConfig): config class that contains the values that are required to generate the directories.
@@ -80,14 +82,15 @@ def prepare_data_directories(
 
     Returns:
         list[str]: returns a list of all directories that were created
-    '''
+    """
     if video_names is None:
         video_names = list()
     data_directories = encoding_config.get_all_result_directories()
 
     for directory in data_directories:
-        directory_path: str = f'{result_root}/{directory}'
+        directory_path: str = f"{result_root}/{directory}"
         Path(directory_path).mkdir(parents=True, exist_ok=True)
+
     return data_directories
 
 
@@ -96,23 +99,28 @@ def get_video_input_files(video_dir: str, encoding_config: EncodingConfig) -> li
         if encoding_config.encode_all_videos:
             return True
 
-        file = file_name.split('.')[0]
+        file = file_name.split(".")[0]
         if USE_SLICED_VIDEOS:
-            file = file.split('_')[0]
-        return encoding_config.videos_to_encode is not None and file in encoding_config.videos_to_encode
+            file = file.split("_")[0]
+        return (
+            encoding_config.videos_to_encode is not None
+            and file in encoding_config.videos_to_encode
+        )
 
-    input_files: list[str] = [file_name for file_name in os.listdir(
-        video_dir) if is_file_in_config(file_name)]
+    input_files: list[str] = [
+        file_name for file_name in os.listdir(video_dir) if is_file_in_config(file_name)
+    ]
 
     if len(input_files) == 0:
-        raise ValueError('no video files to encode')
+        raise ValueError("no video files to encode")
 
     return input_files
 
 
-def get_filtered_sliced_videos(encoding_config: EncodingConfig, input_dir: str) -> list[str]:
-    input_files: list[str] = get_video_input_files(
-        input_dir, encoding_config)
+def get_filtered_sliced_videos(
+    encoding_config: EncodingConfig, input_dir: str
+) -> list[str]:
+    input_files: list[str] = get_video_input_files(input_dir, encoding_config)
 
     return sorted(input_files)
 
@@ -122,7 +130,7 @@ def one_video_multiple_representations_encoding(
     window_size_start: int,
     window_size_end: int,
     input_files: list[str],
-    input_dir: str = INPUT_FILE_DIR
+    input_dir: str = INPUT_FILE_DIR,
 ):
     assert window_size_start > 0
     assert window_size_start < window_size_end
@@ -138,11 +146,14 @@ def one_video_multiple_representations_encoding(
             for framerate in encoding_config.framerate:
                 print(codec, preset, framerate, encoding_config.renditions)
                 for input_file in input_files:
-                    input_file_dir = f'{input_dir}/{input_file}'
+                    input_file_dir = f"{input_dir}/{input_file}"
                     # TODO
                     cmd = create_one_video_multiple_representation_command(
-                        input_file_dir, RESULT_ROOT,
-                        encoding_config, USE_CUDA, pretty_print=False
+                        input_file_dir,
+                        RESULT_ROOT,
+                        encoding_config,
+                        USE_CUDA,
+                        pretty_print=False,
                     )
 
                     print(cmd)
@@ -160,17 +171,13 @@ def multiple_video_one_representation_encoding(
     window_size_start: int,
     window_size_end: int,
     input_files: list[str],
-    input_dir: str = INPUT_FILE_DIR
+    input_dir: str = INPUT_FILE_DIR,
 ) -> None:
-
     assert window_size_start > 0
     assert window_size_start < window_size_end
 
-    encoding_dtos: list[EncodingConfigDTO] = encoding_config.get_encoding_dtos(
-    )
+    encoding_dtos: list[EncodingConfigDTO] = encoding_config.get_encoding_dtos()
     gpu_count = GPU_COUNT
-    
-    
 
     for window_size in range(window_size_start, window_size_end + 1):
         if USE_CUDA and GPU_COUNT > 0:
@@ -185,29 +192,31 @@ def multiple_video_one_representation_encoding(
                 break
 
             input_slice = [
-                f'{input_dir}/{file_slice}' for file_slice in input_files[idx_offset:window_idx]]
+                f"{input_dir}/{file_slice}"
+                for file_slice in input_files[idx_offset:window_idx]
+            ]
 
             for dto in encoding_dtos[:1]:
-                output_directory: str = f'{RESULT_ROOT}/{dto.get_output_directory()}'
+                output_directory: str = f"{RESULT_ROOT}/{dto.get_output_directory()}"
 
                 cmd = create_multi_video_ffmpeg_command(
                     input_slice,
-                    [output_directory]*len(input_slice),
+                    [output_directory] * len(input_slice),
                     dto,
                     cuda_mode=USE_CUDA,
                     gpu_count=gpu_count,
                     quiet_mode=CLI_PARSER.is_quiet_ffmpeg(),
-                    pretty_print=DRY_RUN
+                    pretty_print=DRY_RUN,
                 )
-                
+
                 execute_encoding_cmd(cmd, dto, input_slice)
 
         store_monitoring_results(
-            reset_monitoring_results=True, window_size=window_size * gpu_count)
+            reset_monitoring_results=True, window_size=window_size * gpu_count
+        )
 
 
 def video_cleanup(videos: list[str]) -> None:
-
     for video in videos:
         Path(video).unlink()
 
@@ -217,58 +226,62 @@ def multiple_video_multiple_representations_encoding():
 
 
 def store_monitoring_results(
-    reset_monitoring_results: bool = False,
-    window_size: int = 1
+    reset_monitoring_results: bool = False, window_size: int = 1
 ) -> None:
-
     if len(monitoring_results) > 0:
-        current_time = datetime.now().strftime('%Y-%m-%d_%H-%M')
-        result_path = f'{RESULT_ROOT}/encoding_results_{window_size}_vids_{current_time}_{HOST_NAME}'
+        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M")
+        result_path = f"{RESULT_ROOT}/encoding_results_{window_size}_vids_{current_time}_{HOST_NAME}"
         df = pd.concat(monitoring_results)
-        df.to_parquet(f'{result_path}.parquet', index=True)
+        df.to_parquet(f"{result_path}.parquet", index=True)
 
         if reset_monitoring_results:
             monitoring_results.clear()
     else:
-        print('no monitoring results found')
+        print("no monitoring results found")
 
 
-def execute_encoding_benchmark(encoding_configuration: list[EncodingConfig], parallel_mode: ParallelMode):
-
+def execute_encoding_benchmark(
+    encoding_configuration: list[EncodingConfig], parallel_mode: ParallelMode
+):
     input_dir = INPUT_FILE_DIR
-    input_files = sorted([file for file in os.listdir(
-        INPUT_FILE_DIR) if file.endswith('.mp4') or file.endswith('.265')])
-    
-    assert len(input_files) > 0, 'no input files detected'
+    input_files = sorted(
+        [
+            file
+            for file in os.listdir(INPUT_FILE_DIR)
+            if file.endswith(".mp4") or file.endswith(".265")
+        ]
+    )
+
+    assert len(input_files) > 0, "no input files detected"
 
     for _ in range(TEST_REPETITIONS):
         for _, encoding_config in enumerate(encoding_configuration):
-
-            output_files = [remove_media_extension(
-                out_file) for out_file in input_files]
+            output_files = [
+                remove_media_extension(out_file) for out_file in input_files
+            ]
 
             prepare_data_directories(encoding_config, video_names=output_files)
 
             if parallel_mode == ParallelMode.MULTIPLE_VIDEOS_ONE_REPRESENTATION:
                 multiple_video_one_representation_encoding(
-                    encoding_config, 1, 20, input_files, input_dir)
+                    encoding_config, 1, 20, input_files, input_dir
+                )
 
             elif parallel_mode == ParallelMode.ONE_VIDEO_MULTIPLE_REPRESENTATIONS:
                 one_video_multiple_representations_encoding(
-                    encoding_config, 2, 4, input_files, input_dir)
+                    encoding_config, 2, 4, input_files, input_dir
+                )
 
             elif parallel_mode == ParallelMode.MULTIPLE_VIDEOS_MULTIPLE_REPRESENTATIONS:
                 # TODO
-                raise NotImplementedError('MVMR not implemented yet')
+                raise NotImplementedError("MVMR not implemented yet")
                 # multiple_video_multiple_representations_encoding()
 
     store_monitoring_results()
 
 
 def execute_encoding_cmd(
-        cmd: str,
-        dto: EncodingConfigDTO,
-        input_slice: list[str]
+    cmd: str, dto: EncodingConfigDTO, input_slice: list[str]
 ) -> None:
     if not DRY_RUN:
         hardware_tracker.monitor_process(cmd)
@@ -283,39 +296,45 @@ def add_monitoring_results(dto: EncodingConfigDTO, input_slice: list[str]) -> No
     bitrate, width, height = rendition.bitrate, rendition.width, rendition.height
     framerate, segment_duration = dto.framerate, dto.segment_duration
     result_df = hardware_tracker.to_dataframe()
-    result_df[['preset', 'codec']] = preset, codec
-    result_df[['framerate', 'segment_duration']
-              ] = framerate, segment_duration
-    result_df[['bitrate', 'width', 'height']] = bitrate, width, height
+    result_df[["preset", "codec"]] = preset, codec
+    result_df[["framerate", "segment_duration"]] = framerate, segment_duration
+    result_df[["bitrate", "width", "height"]] = bitrate, width, height
     if USE_CUDA and GPU_COUNT > 0:
         video_list = [
-            f'{abbreviate_video_name(video.split("/")[-1])}_gpu:{idx % GPU_COUNT}' for idx, video in enumerate(input_slice)]
+            f'{abbreviate_video_name(video.split("/")[-1])}_gpu:{idx % GPU_COUNT}'
+            for idx, video in enumerate(input_slice)
+        ]
         for idx in range(len(video_list)):
             idx_mod = idx % GPU_COUNT
-            result_df[f'video_list_gpu:{idx_mod}'] = ','.join([video.removesuffix(
-                f'_gpu:{idx_mod}') for video in video_list if f'gpu:{idx_mod}' in video])
+            result_df[f"video_list_gpu:{idx_mod}"] = ",".join(
+                [
+                    video.removesuffix(f"_gpu:{idx_mod}")
+                    for video in video_list
+                    if f"gpu:{idx_mod}" in video
+                ]
+            )
     else:
-        result_df['video_list'] = ','.join(
-            [abbreviate_video_name(video.split('/')[-1]) for video in input_slice])
-    result_df['num_videos'] = len(input_slice)
+        result_df["video_list"] = ",".join(
+            [abbreviate_video_name(video.split("/")[-1]) for video in input_slice]
+        )
+    result_df["num_videos"] = len(input_slice)
 
     monitoring_results.append(result_df)
     hardware_tracker.clear()
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     Path(RESULT_ROOT).mkdir(parents=True, exist_ok=True)
-    Path(f'{RESULT_ROOT}/tmp').mkdir(parents=True, exist_ok=True)
+    Path(f"{RESULT_ROOT}/tmp").mkdir(parents=True, exist_ok=True)
 
-    encoding_configs: list[EncodingConfig] = [EncodingConfig.from_file(
-        file_path) for file_path in ENCODING_CONFIG_PATHS]
+    encoding_configs: list[EncodingConfig] = [
+        EncodingConfig.from_file(file_path) for file_path in ENCODING_CONFIG_PATHS
+    ]
 
     hardware_tracker.start()
 
     pm = ParallelMode.MULTIPLE_VIDEOS_ONE_REPRESENTATION
 
-    execute_encoding_benchmark(
-        encoding_configs, pm)
+    execute_encoding_benchmark(encoding_configs, pm)
 
     hardware_tracker.stop()
